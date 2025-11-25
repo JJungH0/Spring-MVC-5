882ec34e3fce197459351605bc8d63017cdab3e7
JJung0 <junghwan__@naver.com>
2025-11-25 18:40:03 +0900

    [FEAT]: 빌드에러 해결

--- src/main/java/hello/itemservice/TestDataInit.java
+++ src/main/java/hello/itemservice/TestDataInit.java
@@ -2,10 +2,11 @@ package hello.itemservice;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
+//import javax.annotation.PostConstruct;
 
 @Component
 @RequiredArgsConstructor
